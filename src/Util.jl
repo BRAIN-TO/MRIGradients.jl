1ced0653d26d35d13c476273e7cb1e0e899a4d72
Alexander Jaffray <alexander.jaffray@gmail.com>
2022-08-16 09:35:55 -0700

    added output arguments and sizes to gradient-node conversion function docs

--- src/Util.jl
+++ src/Util.jl
@@ -277,11 +277,13 @@ end
 Function to calculate the gradient train used to acquire k-space data
 
 # Arguments
-* `nodes::Matrix` - input k-space nodes traversed in order, acquired with constant dwell time. 
-* `gamma` - gyromagnetic ratio
-* `dwellTime` - the dwell time of the trajectory
-* `FOV` - spatial field of view in [mm]
-* `reconSize` - reconstruction matrix size
+* `nodes::Matrix` - input k-space nodes traversed in order, acquired with constant dwell time. Size: (`2` x `N`) where N is the number of nodes
+* `gamma` - gyromagnetic ratio [Hz]
+* `dwellTime` - the dwell time of the trajectory [s]
+* `FOV` - spatial field of view in [mm] [`1` x `3`]
+* `reconSize` - reconstruction matrix size [`1` x `3`]
+# Outputs
+* `gradients::Matrix` - gradients corresponding to k-space samples. Size: (`2` x `N`) where N is the number of k-space samples
 """
 function nodes_to_gradients(nodes::Matrix; gamma=42577478, dwellTime=2e-6, FOV=[220,220,1],reconSize=[200,200,1])
 
@@ -301,11 +303,13 @@ end
 Function to calculate the k-space nodes from an applied gradient train
 
 # Arguments
-* `gradients::Matrix` - input gradients played out, acquired with constant dwell time. 
-* `gamma` - gyromagnetic ratio
-* `dwellTime` - the dwell time of the trajectory
-* `FOV` - spatial field of view in [mm]
-* `reconSize` - reconstruction matrix size
+* `gradients::Matrix` - input gradients played out, acquired with constant dwell time. Size: (`2` x `N`) where N is the number of gradient samples
+* `gamma` - gyromagnetic ratio [Hz]
+* `dwellTime` - the dwell time of the trajectory [s]
+* `FOV` - spatial field of view in [mm] [`1` x `3`]
+* `reconSize` - reconstruction matrix size [`1` x `3`]
+# Outputs
+* `nodes::Matrix` - k-space nodes corresponding to applied gradient train. Size: (`2` x `N`) where N is the number of gradient samples
 """
 function gradients_to_nodes(gradients::Matrix; gamma=42577478, dwellTime=2e-6, FOV=[220,220,1],reconSize=[200,200,1])
 
